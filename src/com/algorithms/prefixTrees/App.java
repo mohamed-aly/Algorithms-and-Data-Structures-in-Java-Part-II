b020b472359ac65eb9a2a99f9302bcfb3d8393cf
mohamed-aly <mali902016@gmail.com>
2021-06-21 17:27:07 +0200

    012 Prefix tree implementation - insert_ search III

--- src/com/algorithms/prefixTrees/App.java
+++ src/com/algorithms/prefixTrees/App.java
@@ -1,4 +1,13 @@
 package com.algorithms.prefixTrees;
 
 public class App {
+    public static void main(String[] args) {
+        Trie trie = new Trie();
+        trie.insert("Mohamed");
+        trie.insert("ahmed");
+        trie.insert("aly");
+        trie.insert("elena");
+
+        System.out.println(trie.has("Mohamed"));
+    }
 }
--- src/com/algorithms/prefixTrees/Trie.java
+++ src/com/algorithms/prefixTrees/Trie.java
@@ -1,5 +1,7 @@
 package com.algorithms.prefixTrees;
 
+import java.util.Arrays;
+
 public class Trie {
     private Node root;
 
@@ -10,21 +12,38 @@ public class Trie {
     public void insert(String key) {
         Node tempNode = this.root;
 
-        for(int i = 0; i<key.length(); i++){
+        for (int i = 0; i < key.length(); i++) {
             key = key.toLowerCase();
             char c = key.charAt(i);
-            int asciiIndex = c-'a';
+            int asciiIndex = c - 'a';
 
-            if(tempNode.getChild(asciiIndex) == null){
+            if (tempNode.getChild(asciiIndex) == null) {
                 Node node = new Node(String.valueOf(c));
                 tempNode.addChild(asciiIndex, node);
                 tempNode = node;
-            }else{
+            } else {
                 tempNode = tempNode.getChild(asciiIndex);
             }
         }
 
         tempNode.setLeaf(true);
+    }
+
+    public boolean has(String key) {
+        key = key.toLowerCase();
+        Node tempNode = this.root;
+
+        for (int counter = 0; counter < key.length(); counter++) {
+            char c = key.charAt(counter);
+            int asciiIndex = c - 'a';
+            if (tempNode.getChild(asciiIndex) == null) {
+                return false;
+            } else {
+                tempNode = tempNode.getChild(asciiIndex);
+            }
+        }
+
+        return tempNode.isLeaf();
     }
 
 }
