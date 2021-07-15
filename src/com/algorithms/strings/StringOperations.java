5976391b9ad4e055dfb6422619e60ed79bb75e88
mohamed-aly <mali902016@gmail.com>
2021-07-15 14:11:48 +0200

    030-2 evaluate expression with single-operation bracket with recursion

--- src/com/algorithms/strings/StringOperations.java
+++ src/com/algorithms/strings/StringOperations.java
@@ -15,58 +15,62 @@ public class StringOperations {
 
         expression = expression.replace(" ", "");
 
+
+
+        return replaceSyllablesWithEvaluations(expression);
+    }
+
+    private static String replaceSyllablesWithEvaluations(String expression) {
+        //"5 / 5 + (3 - 2) * 6 / 6"
         while (getOperatorIndex(expression) > 0) {
-            expression = replaceSyllablesWithEvaluations(expression, getOperatorIndex(expression));
+
+            int operatorIndex = getOperatorIndex(expression);
+            char operator = expression.charAt(operatorIndex);
+            boolean isBracketsSyllable = false;
+
+            if (operator == '(') {
+                isBracketsSyllable = true;
+                int endBracketIndex = expression.indexOf(')');
+                String bracketsSyllable = expression.substring(operatorIndex + 1, endBracketIndex);
+                operatorIndex = getOperatorIndex(bracketsSyllable) + operatorIndex + 1;
+                operator = expression.charAt(operatorIndex);
+            }
+
+            StringBuilder firstOperandBuilder = new StringBuilder();
+            StringBuilder secondOperandBuilder = new StringBuilder();
+            for (int i = operatorIndex - 1; i >= 0; i--) {
+                if (isOperand(expression.charAt(i))) {
+                    firstOperandBuilder.append(expression.charAt(i));
+                } else {
+                    break;
+                }
+            }
+
+            firstOperandBuilder.reverse();
+
+            for (int i = operatorIndex + 1; i < expression.length(); i++) {
+                if (isOperand(expression.charAt(i))) {
+                    secondOperandBuilder.append(expression.charAt(i));
+                } else {
+                    break;
+                }
+            }
+
+            String firstOperand = firstOperandBuilder.toString();
+            String secondOperand = secondOperandBuilder.toString();
+
+            String syllable;
+            if (isBracketsSyllable) {
+                syllable = '(' + firstOperand + operator + secondOperand + ')';
+            } else {
+                syllable = firstOperand + operator + secondOperand;
+            }
+
+            double evaluation = calculateSyllable(Double.parseDouble(firstOperand), Double.parseDouble(secondOperand), operator);
+            expression = expression.replace(syllable, String.valueOf(evaluation));
         }
 
         return expression;
-    }
-
-    private static String replaceSyllablesWithEvaluations(String expression, int operatorIndex) {
-        //"5 / 5 + (3 - 2) * 6 / 6"
-        char operator = expression.charAt(operatorIndex);
-        boolean isBracketsSyllable = false;
-
-        if (operator == '(') {
-            isBracketsSyllable = true;
-            int endBracketIndex = expression.indexOf(')');
-            String bracketsSyllable = expression.substring(operatorIndex + 1, endBracketIndex);
-            operatorIndex = getOperatorIndex(bracketsSyllable) + operatorIndex + 1;
-            operator = expression.charAt(operatorIndex);
-        }
-
-        StringBuilder firstOperandBuilder = new StringBuilder();
-        StringBuilder secondOperandBuilder = new StringBuilder();
-        for (int i = operatorIndex - 1; i >= 0; i--) {
-            if (isOperand(expression.charAt(i))) {
-                firstOperandBuilder.append(expression.charAt(i));
-            } else {
-                break;
-            }
-        }
-
-        firstOperandBuilder.reverse();
-
-        for (int i = operatorIndex + 1; i < expression.length(); i++) {
-            if (isOperand(expression.charAt(i))) {
-                secondOperandBuilder.append(expression.charAt(i));
-            } else {
-                break;
-            }
-        }
-
-        String firstOperand = firstOperandBuilder.toString();
-        String secondOperand = secondOperandBuilder.toString();
-
-        String syllable;
-        if (isBracketsSyllable) {
-            syllable = '(' + firstOperand + operator + secondOperand + ')';
-        }else{
-            syllable = firstOperand + operator + secondOperand;
-        }
-
-        double evaluation = calculateSyllable(Double.parseDouble(firstOperand), Double.parseDouble(secondOperand), operator);
-        return expression.replace(syllable, String.valueOf(evaluation));
     }
 
     private static double calculateSyllable(double firstOperand, double secondOperand, char operator) {
