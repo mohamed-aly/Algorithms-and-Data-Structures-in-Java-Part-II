58873b4aaa2eb975cf55ea3e6097bf5318e788a7
mohamed-aly <mali902016@gmail.com>
2021-07-15 09:58:46 +0200

    030-2 evaluate expression with multiple digits

--- src/com/algorithms/strings/StringOperations.java
+++ src/com/algorithms/strings/StringOperations.java
@@ -10,70 +10,80 @@ public class StringOperations {
         return stringBuilder.toString();
     }
 
+
     public static String evaluate(String expression) {
         //"5 / 5 + 3 - 2 * 6 / 6"
         expression = expression.replace(" ", "");
-        while (operatorIndex(expression) > 0) {
-            expression = replaceSyllableWithEvaluation(expression, operatorIndex(expression));
+
+        while (getOperatorIndex(expression) > 0) {
+            expression = replaceSyllablesWithEvaluations(expression, getOperatorIndex(expression));
         }
 
         return expression;
-
     }
 
-    private static int operatorIndex(String expression) {
-        if (expression.indexOf("/") > 0) {
-            return expression.indexOf("/");
-        } else if (expression.indexOf("*") > 0) {
-            return expression.indexOf("*");
-        } else if (expression.indexOf("+") > 0) {
-            return expression.indexOf("+");
-        } else if (expression.indexOf("-") > 0) {
-            return expression.indexOf("-");
+    private static String replaceSyllablesWithEvaluations(String expression, int operatorIndex) {
+        StringBuilder firstOperandBuilder = new StringBuilder();
+        StringBuilder secondOperandBuilder = new StringBuilder();
+
+        for (int i = operatorIndex - 1; i >= 0; i--) {
+            if (isOperand(expression.charAt(i))) {
+                firstOperandBuilder.append(expression.charAt(i));
+            }else{
+                break;
+            }
+        }
+        
+        firstOperandBuilder.reverse();
+        
+        for(int i = operatorIndex+1; i<expression.length(); i++){
+            if (isOperand(expression.charAt(i))) {
+                secondOperandBuilder.append(expression.charAt(i));
+            }else{
+                break;
+            }
+        }
+        
+        String firstOperand = firstOperandBuilder.toString();
+        String secondOperand = secondOperandBuilder.toString();
+        char operator = expression.charAt(operatorIndex);
+
+        String syllable = firstOperand + operator + secondOperand;
+        double evaluation = calculateSyllable(Double.parseDouble(firstOperand), Double.parseDouble(secondOperand), operator);
+        return expression.replace(syllable, String.valueOf(evaluation));
+    }
+
+    private static double calculateSyllable(double firstOperand, double secondOperand, char operator) {
+        double evaluation;
+        switch (operator){
+            case '/': evaluation = firstOperand / secondOperand; break;
+            case '*': evaluation = firstOperand * secondOperand; break;
+            case '+': evaluation = firstOperand + secondOperand; break;
+            case '-': evaluation = firstOperand - secondOperand; break;
+            default:
+                throw new IllegalStateException("Unexpected value: " + operator);
+        }
+        
+        return evaluation;
+    }
+
+    private static boolean isOperand(char c) {
+        return (c >= '0' && c <= '9') || c == '.';
+    }
+
+    private static int getOperatorIndex(String expression) {
+        if (expression.contains("/")) {
+            return expression.indexOf('/');
+        } else if (expression.contains("*")) {
+            return expression.indexOf('*');
+        } else if (expression.contains("+")) {
+            return expression.indexOf('+');
+        } else if (expression.contains("-")) {
+            return expression.indexOf('-');
         }
 
         return -1;
     }
 
-    private static String replaceSyllableWithEvaluation(String expresion, int operatorIndex) {
-        String syllable;
-        if (operatorIndex >= expresion.length() - 2) {
-            syllable = expresion.substring(operatorIndex - 1);
-        } else {
-            syllable = expresion.substring(operatorIndex - 1, operatorIndex + 2);
-        }
 
-        return expresion.replace(syllable, calculateSyllable(syllable));
-
-    }
-
-    private static String calculateSyllable(String syllable) {
-        int firstOperand = operandValue(syllable.charAt(0));
-        char operator = syllable.charAt(1);
-        int secondOperand = operandValue(syllable.charAt(2));
-        int sum;
-
-        switch (operator) {
-            case '/':
-                sum = firstOperand / secondOperand;
-                break;
-            case '*':
-                sum = firstOperand * secondOperand;
-                break;
-            case '+':
-                sum = firstOperand + secondOperand;
-                break;
-            case '-':
-                sum = firstOperand - secondOperand;
-                break;
-            default:
-                sum = -1;
-        }
-
-        return String.valueOf(sum);
-    }
-
-    private static int operandValue(char c) {
-        return c - '0';
-    }
 }
