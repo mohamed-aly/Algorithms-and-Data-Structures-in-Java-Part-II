6b2864536dea0738184b1cc14c9084ee894aa5be
mohamed-aly <mali902016@gmail.com>
2021-07-25 13:40:49 +0200

    036 Longest common prefix

--- src/com/algorithms/strings/StringOperations.java
+++ src/com/algorithms/strings/StringOperations.java
@@ -25,6 +25,18 @@ public class StringOperations {
         return prefixes;
     }
 
+    public static String longestCommonPrefix(String text1, String text2){
+        int commonLength = Math.min(text1.length(), text2.length());
+
+        for(int index = 0; index<commonLength; index++){
+            if(text1.charAt(index) != text2.charAt(index)){
+                return text1.substring(0, index);
+            }
+        }
+
+        return text2.substring(0, commonLength);
+    }
+
     public static String reverseString(String text) {
         StringBuilder stringBuilder = new StringBuilder();
         for (int i = text.length() - 1; i >= 0; i--) {
